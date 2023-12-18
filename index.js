31cc06d6f19be74969e817abfae60686d3a3a361
mitrofazxc <mitrofanzxc@gmail.com>
2023-12-18 18:35:57 +0300

    refactor(*): dependency update

--- index.js
+++ index.js
@@ -2,35 +2,34 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const main = async () => {
-    try {
-        const owner = core.getInput('owner', { required: true });
-        const repo = core.getInput('repo', { required: true });
-        const token = core.getInput('token', { required: true });
-        const octokit = new github.getOctokit(token);
+  try {
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const token = core.getInput('token', { required: true });
+    const octokit = new github.getOctokit(token);
 
-        await octokit.rest.teams.addOrUpdateRepoPermissionsInOrg({
-            org: owner,
-            team_slug: 'Mentors',
-            owner,
-            repo,
-            permission: 'push'
-        });
+    await octokit.rest.teams.addOrUpdateRepoPermissionsInOrg({
+      org: owner,
+      team_slug: 'Mentors',
+      owner,
+      repo,
+      permission: 'push',
+    });
 
-        await octokit.rest.repos.updateBranchProtection({
-            owner,
-            repo,
-            branch: 'main',
-            required_status_checks: null,
-            enforce_admins: false,
-            required_pull_request_reviews: {
-                required_approving_review_count: 2
-            },
-            restrictions: null,
-        });
-
-    } catch (error) {
-        core.setFailed(error.message);
-    }
-}
+    await octokit.rest.repos.updateBranchProtection({
+      owner,
+      repo,
+      branch: 'main',
+      required_status_checks: null,
+      enforce_admins: false,
+      required_pull_request_reviews: {
+        required_approving_review_count: 2,
+      },
+      restrictions: null,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
 
 main();
